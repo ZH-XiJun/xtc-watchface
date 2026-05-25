f0c93386e2ac795e1277e4a70222bcbe59b9bc3d
zh-xijun <59949135+ZH-XiJun@users.noreply.github.com>
2026-05-25 22:07:05 +0800

    fix: convert AAR manifest to text XML, bump compileSdk to 36

---
+++ wonderland/src/main/java/com/xtc/common/WallpaperServiceProxy.java
@@ -0,0 +1,29 @@
+package com.xtc.common;
+
+import android.content.Context;
+import android.service.wallpaper.WallpaperService;
+import android.util.Log;
+
+/* loaded from: classes3.dex */
+public class WallpaperServiceProxy extends WallpaperService {
+    public WallpaperServiceProxy(Context context, String path) {
+        attachBaseContext(context);
+        Log.d("WallpaperServiceProxy", "zjj, WallpaperServiceProxy2");
+    }
+
+    public WallpaperServiceProxy(Context context) {
+        attachBaseContext(context);
+        Log.d("WallpaperServiceProxy", "zjj, WallpaperServiceProxy");
+    }
+
+    @Override // android.service.wallpaper.WallpaperService
+    public WallpaperService.Engine onCreateEngine() {
+        return null;
+    }
+
+    public class ActiveEngine extends WallpaperService.Engine {
+        public ActiveEngine() {
+            super(WallpaperServiceProxy.this);
+        }
+    }
+}
--- wonderland/src/main/java/com/xtc/wonderland/WallpaperServiceImpl.java
+++ wonderland/src/main/java/com/xtc/wonderland/WallpaperServiceImpl.java
@@ -3,52 +3,37 @@ package com.xtc.wonderland;
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.os.Handler;
 import android.util.Log;
 import android.view.SurfaceHolder;
-
-import com.xtc.common.WallpaperServiceProxy.ActiveEngine;
-import com.xtc.common.WeakHandler;
+import com.xtc.common.WallpaperServiceProxy;
 import com.xtc.common.WatchDataBase;
+import com.xtc.common.WeakHandler;
+import com.xtc.wonderland.XTCBaseWallpaperService2;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
+/* loaded from: classes2.dex */
 public class WallpaperServiceImpl extends XTCBaseWallpaperService2 {
     public static final int ANIMATION_COUNT = 20;
     public static final int ANIMATION_INTERVAL = 40;
     public static final int REFRESH_INTERVAL = 3000;
     public static final String TAG = "WallpaperServiceImpl_wonderland";
-
     private String path;
     private Resources resources;
 
     public WallpaperServiceImpl(Context context, String path) {
         super(context);
         this.path = path;
-        this.resources = initResources(context);
-    }
-
-    static Resources access$000(WallpaperServiceImpl instance) {
-        return instance.resources;
-    }
-
-    public Resources initResources(Context context) {
         try {
-            AssetManager assetManager = (AssetManager) AssetManager.class.newInstance();
-            assetManager.getClass()
-                    .getMethod("addAssetPath", String.class)
-                    .invoke(assetManager, path);
-            Resources res = context.getResources();
-            return new Resources(assetManager,
-                    res.getDisplayMetrics(),
-                    res.getConfiguration());
-        } catch (Exception e) {
-            e.printStackTrace();
-            return getResources();
-        }
+            this.resources = initResources(context);
+        } catch (Exception ignore) {}
     }
 
-    @Override
-    public ActiveEngine onCreateEngine() {
+    @Override // com.xtc.wonderland.XTCBaseWallpaperService2, com.xtc.common.WallpaperServiceProxy, android.service.wallpaper.WallpaperService
+    public WallpaperServiceProxy.ActiveEngine onCreateEngine() {
         return new WonderlandEngine();
     }
 
@@ -59,178 +44,191 @@ public class WallpaperServiceImpl extends XTCBaseWallpaperService2 {
         private boolean visible;
 
         public WonderlandEngine() {
-            animationHandler = new WeakHandler();
-            refreshHandler = new WeakHandler();
+            super();
+            this.animationHandler = new WeakHandler();
+            this.refreshHandler = new WeakHandler();
         }
 
-        @Override
-        public void onCreate(SurfaceHolder surfaceHolder) {
-            super.onCreate(surfaceHolder);
-            Log.i(TAG, "onCreate:");
-            staticWallPaperRender = new WallpaperRender(
-                    WallpaperServiceImpl.this.resources,
-                    WallpaperServiceImpl.this.getApplicationContext());
-            staticWallPaperRender.setBatteryLevel(
-                    WatchDataBase.getWatchBatteryLevel(
-                            WallpaperServiceImpl.this.getApplicationContext()));
+        @Override // com.xtc.wonderland.XTCBaseWallpaperService2.BaseEngine, android.service.wallpaper.WallpaperService.Engine
+        public void onCreate(SurfaceHolder holder) {
+            super.onCreate(holder);
+            Log.i(WallpaperServiceImpl.TAG, "onCreate:");
+            this.staticWallPaperRender = new WallpaperRender(WallpaperServiceImpl.this.resources, WallpaperServiceImpl.this.getApplicationContext());
+            this.staticWallPaperRender.setBatteryLevel(WatchDataBase.getWatchBatteryLevel(WallpaperServiceImpl.this.getApplicationContext()));
         }
 
-        @Override
+        @Override // com.xtc.wonderland.XTCBaseWallpaperService2.BaseEngine, com.xtc.common.callback.IDialCallback
+        public void showWallpaper() {
+            super.showWallpaper();
+            Log.i(WallpaperServiceImpl.TAG, "showWallpaper");
+            playAnimation();
+        }
+
+        private void playAnimation() {
+            Log.i(WallpaperServiceImpl.TAG, "start play animation");
+            this.staticWallPaperRender.setAnimationStep(0);
+            drawFrame();
+            this.animationHandler.removeCallbacksAndMessages(null);
+            this.animationHandler.post(createAnimationTask());
+        }
+
+        private void startRefreshDial() {
+            this.refreshHandler.removeCallbacksAndMessages(null);
+            this.refreshHandler.postDelayed(createRefreshTask(), 3000L);
+        }
+
+        private void stopRefresh() {
+            WeakHandler weakHandler = this.refreshHandler;
+            if (weakHandler != null) {
+                weakHandler.removeCallbacksAndMessages(null);
+            }
+        }
+
+        /* JADX INFO: Access modifiers changed from: private */
+        public Runnable createRefreshTask() {
+            Runnable runnable = new Runnable() { // from class: com.xtc.wonderland.WallpaperServiceImpl.WonderlandEngine.1
+                @Override // java.lang.Runnable
+                public void run() {
+                    if (WonderlandEngine.this.refreshHandler != null) {
+                        WonderlandEngine.this.refreshHandler.removeCallbacksAndMessages(null);
+                        WonderlandEngine.this.drawFrame();
+                        WonderlandEngine.this.refreshHandler.postDelayed(WonderlandEngine.this.createRefreshTask(), 3000L);
+                    }
+                }
+            };
+            return runnable;
+        }
+
+        /* JADX INFO: Access modifiers changed from: private */
+        public Runnable createAnimationTask() {
+            Runnable runnable = new Runnable() { // from class: com.xtc.wonderland.WallpaperServiceImpl.WonderlandEngine.2
+                @Override // java.lang.Runnable
+                public void run() {
+                    Log.i(WallpaperServiceImpl.TAG, "run");
+                    if (WonderlandEngine.this.animationHandler != null) {
+                        WonderlandEngine.this.animationHandler.removeCallbacksAndMessages(null);
+                        if (WonderlandEngine.this.staticWallPaperRender.getAnimationStep() >= 20) {
+                            WonderlandEngine.this.staticWallPaperRender.setAnimationStep(-1);
+                            WonderlandEngine.this.drawFrame();
+                            Log.i(WallpaperServiceImpl.TAG, ">");
+                            return;
+                        }
+                        Log.i(WallpaperServiceImpl.TAG, "run step:" + WonderlandEngine.this.staticWallPaperRender.getAnimationStep());
+                        WonderlandEngine.this.animationHandler.postDelayed(WonderlandEngine.this.createAnimationTask(), 40L);
+                        WonderlandEngine.this.drawFrame();
+                        int step = WonderlandEngine.this.staticWallPaperRender.getAnimationStep() + 1;
+                        WonderlandEngine.this.staticWallPaperRender.setAnimationStep(step);
+                        return;
+                    }
+                    Log.i(WallpaperServiceImpl.TAG, "null");
+                }
+            };
+            return runnable;
+        }
+
+        @Override // com.xtc.wonderland.XTCBaseWallpaperService2.BaseEngine, com.xtc.common.callback.IDialCallback
+        public void updateBattery(int batteryLevel) {
+            super.updateBattery(batteryLevel);
+            this.staticWallPaperRender.setBatteryLevel(batteryLevel);
+        }
+
+        @Override // com.xtc.wonderland.XTCBaseWallpaperService2.BaseEngine, android.service.wallpaper.WallpaperService.Engine
         public void onSurfaceCreated(SurfaceHolder holder) {
-            isNeedBattery = true;
+            this.isNeedBattery = true;
             super.onSurfaceCreated(holder);
-            Log.i(TAG, "onSurfaceCreated:");
+            Log.i(WallpaperServiceImpl.TAG, "onSurfaceCreated: ");
             drawFrame();
             playAnimation();
         }
 
-        @Override
+        @Override // android.service.wallpaper.WallpaperService.Engine
         public void onSurfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-            Log.i(TAG, "onSurfaceChanged:");
+            Log.i(WallpaperServiceImpl.TAG, "onSurfaceChanged: ");
             drawFrame();
         }
 
-        @Override
+        @Override // android.service.wallpaper.WallpaperService.Engine
         public void onSurfaceDestroyed(SurfaceHolder holder) {
             super.onSurfaceDestroyed(holder);
-            Log.i(TAG, "onSurfaceDestroyed:" + visible);
-            visible = false;
+            Log.i(WallpaperServiceImpl.TAG, "onSurfaceDestroyed:" + this.visible);
+            this.visible = false;
         }
 
-        @Override
-        public void onVisibilityChanged(boolean visible) {
-            this.visible = visible;
-            Log.i(TAG, "onVisibilityChanged:" + this.visible);
+        @Override // com.xtc.wonderland.XTCBaseWallpaperService2.BaseEngine, android.service.wallpaper.WallpaperService.Engine
+        public void onDestroy() {
+            super.onDestroy();
+            Log.i(WallpaperServiceImpl.TAG, "onDestroy:" + this.visible);
+            this.visible = false;
+        }
+
+        @Override // com.xtc.wonderland.XTCBaseWallpaperService2.BaseEngine, android.service.wallpaper.WallpaperService.Engine
+        public void onVisibilityChanged(boolean isVisible) {
+            this.visible = isVisible;
+            Log.i(WallpaperServiceImpl.TAG, "onVisibilityChanged:" + this.visible);
             if (this.visible) {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
+                new Handler().postDelayed(new Runnable() { // from class: com.xtc.wonderland.WallpaperServiceImpl.WonderlandEngine.3
+                    @Override // java.lang.Runnable
                     public void run() {
-                        drawFrame();
+                        WonderlandEngine.this.drawFrame();
                     }
-                }, 200);
+                }, 200L);
                 startRefreshDial();
             } else {
                 stopRefresh();
             }
         }
 
-        @Override
-        public void onDestroy() {
-            super.onDestroy();
-            Log.i(TAG, "onDestroy:" + visible);
-            visible = false;
-        }
-
-        @Override
-        public void showWallpaper() {
-            super.showWallpaper();
-            Log.i(TAG, "showWallpaper");
-            playAnimation();
-        }
-
-        @Override
+        @Override // com.xtc.wonderland.XTCBaseWallpaperService2.BaseEngine, com.xtc.common.callback.IDialCallback
         public void offScreen() {
             super.offScreen();
             clearScreen();
-        }
-
-        @Override
-        public void updateBattery(int level) {
-            super.updateBattery(level);
-            if (staticWallPaperRender != null) {
-                staticWallPaperRender.setBatteryLevel(level);
-            }
-        }
-
-        private void drawFrame() {
-            drawFrame(false);
-        }
-
-        private void drawFrame(boolean clean) {
-            Log.i(TAG, "drawFrame clean" + clean);
-            if (!visible && !clean) {
-                return;
-            }
-            SurfaceHolder surfaceHolder = getSurfaceHolder();
-            Canvas canvas = null;
-            try {
-                canvas = surfaceHolder.lockCanvas();
-                if (clean) {
-                    canvas.drawColor(0xFF000000);
-                } else if (staticWallPaperRender != null) {
-                    staticWallPaperRender.onDraw(canvas);
-                }
-                if (canvas != null && surfaceHolder.getSurface().isValid()) {
-                    surfaceHolder.unlockCanvasAndPost(canvas);
-                }
-            } catch (Exception e) {
-                if (canvas != null && surfaceHolder.getSurface().isValid()) {
-                    surfaceHolder.unlockCanvasAndPost(canvas);
-                }
-                throw e;
-            }
         }
 
         private void clearScreen() {
             drawFrame(true);
         }
 
-        private void playAnimation() {
-            Log.i(TAG, "start play animation");
-            staticWallPaperRender.setAnimationStep(0);
-            drawFrame();
-            animationHandler.removeCallbacksAndMessages(null);
-            animationHandler.post(createAnimationTask());
+        /* JADX INFO: Access modifiers changed from: private */
+        public void drawFrame() {
+            drawFrame(false);
         }
 
-        private Runnable createAnimationTask() {
-            return new Runnable() {
-                @Override
-                public void run() {
-                    Log.i(TAG, "run");
-                    if (animationHandler == null) {
-                        Log.i(TAG, "null");
-                        return;
-                    }
-                    animationHandler.removeCallbacksAndMessages(null);
-                    if (staticWallPaperRender.getAnimationStep() >= ANIMATION_COUNT) {
-                        staticWallPaperRender.setAnimationStep(-1);
-                        drawFrame();
-                        Log.i(TAG, ">");
-                        return;
-                    }
-                    Log.i(TAG, "run step:" + staticWallPaperRender.getAnimationStep());
-                    animationHandler.postDelayed(createAnimationTask(), ANIMATION_INTERVAL);
-                    drawFrame();
-                    int step = staticWallPaperRender.getAnimationStep();
-                    staticWallPaperRender.setAnimationStep(step + 1);
-                }
-            };
-        }
-
-        private Runnable createRefreshTask() {
-            return new Runnable() {
-                @Override
-                public void run() {
-                    if (refreshHandler == null) {
-                        return;
-                    }
-                    refreshHandler.removeCallbacksAndMessages(null);
-                    drawFrame();
-                    refreshHandler.postDelayed(createRefreshTask(), REFRESH_INTERVAL);
-                }
-            };
-        }
-
-        private void startRefreshDial() {
-            refreshHandler.removeCallbacksAndMessages(null);
-            refreshHandler.postDelayed(createRefreshTask(), REFRESH_INTERVAL);
-        }
-
-        private void stopRefresh() {
-            if (refreshHandler != null) {
-                refreshHandler.removeCallbacksAndMessages(null);
+        private void drawFrame(boolean clean) {
+            Log.i(WallpaperServiceImpl.TAG, "drawFrame clean" + clean);
+            if (!this.visible && !clean) {
+                return;
             }
+            SurfaceHolder mHolder = getSurfaceHolder();
+            Canvas canvas = null;
+            Bitmap bitmap = null;
+            try {
+                canvas = mHolder.lockCanvas();
+                if (clean) {
+                    canvas.drawColor(-16777216);
+                } else if (this.staticWallPaperRender != null) {
+                    this.staticWallPaperRender.onDraw(canvas);
+                }
+            } finally {
+                if (canvas != null && mHolder.getSurface().isValid()) {
+                    mHolder.unlockCanvasAndPost(canvas);
+                }
+                if (0 != 0) {
+                    bitmap.recycle();
+                }
+            }
+        }
+    }
+
+    public Resources initResources(Context context) throws IllegalAccessException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
+        try {
+            AssetManager e = (AssetManager) AssetManager.class.newInstance();
+            Method addAssetPath = e.getClass().getMethod("addAssetPath", String.class);
+            addAssetPath.invoke(e, this.path);
+            Resources superRes = context.getResources();
+            return new Resources(e, superRes.getDisplayMetrics(), superRes.getConfiguration());
+        } catch (Exception var5) {
+            var5.printStackTrace();
+            return getResources();
         }
     }
 }
